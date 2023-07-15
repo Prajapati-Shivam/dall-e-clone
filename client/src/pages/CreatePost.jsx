d40cf1e9f39e46c5c487ce38672d2e458d3601fc
Prajapati-Shivam <prajapatishivam11203@gmail.com>
2023-07-15 20:55:59 +0530

    changed backend host

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -72,7 +72,7 @@ const CreatePost = () => {
     if (form.name && form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
+        const response = await fetch("https://imagery-uri5.onrender.com/api/v1/post", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
