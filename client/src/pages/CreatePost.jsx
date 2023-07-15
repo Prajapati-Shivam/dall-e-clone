5dfb3b637456f898a7829275938bde4e2988736f
Prajapati-Shivam <prajapatishivam11203@gmail.com>
2023-07-15 20:00:42 +0530

    posting image

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -14,31 +14,80 @@ const CreatePost = () => {
   });
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [alert, setAlert] = useState("");
+
+  // const generateImage = async () => {
+  //   if (form.prompt) {
+  //     try {
+  //       setGeneratingImg(true);
+  //       const response = await fetch("http://localhost:8080/api/v1/create", {
+  //         method: "POST",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //         body: JSON.stringify({ prompt: form.prompt }),
+  //       });
+  //       const data = await response.json();
+  //       setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+  //     } catch (error) {
+  //       alert(error);
+  //     } finally {
+  //       setGeneratingImg(false);
+  //     }
+  //   } else {
+  //     alert("Please enter a prompt");
+  //   }
+  // };
 
   const generateImage = async () => {
     if (form.prompt) {
+      setGeneratingImg(true);
+      const url = "https://open-ai21.p.rapidapi.com/dalle";
+      const options = {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          "X-RapidAPI-Key": "1c4d7492a0msh14ac9b1a80f7bf6p1397d7jsn5c1c398f8857",
+          "X-RapidAPI-Host": "open-ai21.p.rapidapi.com",
+        },
+        body: JSON.stringify({ text: form.prompt }),
+      };
+
       try {
-        setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/create', {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ prompt: form.prompt })
-        })
-        const data = await response.json();
-        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+        const response = await fetch(url, options);
+        const result = await response.json();
+        setForm({ ...form, photo: result.url });
       } catch (error) {
-        alert(error);
+        console.error(error);
       } finally {
         setGeneratingImg(false);
       }
     } else {
-      alert("Please enter a prompt");
+      setAlert("Please enter a prompt");
     }
   };
 
-  const handleSubmit = () => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (form.name && form.prompt && form.photo) {
+      setLoading(true);
+      try {
+        const response = await fetch("http://localhost:8080/api/v1/post", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(form),
+        });
+        await response.json();
+        navigate("/");
+      } catch (error) {
+        setAlert(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+  };
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -60,7 +109,27 @@ const CreatePost = () => {
           share with the community.
         </p>
       </div>
-
+      {alert && (
+        <div className="mt-5 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative">
+          <span className="block sm:inline">{alert}</span>
+          <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+            <svg
+              className="fill-current h-6 w-6 text-red-500"
+              role="button"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              onClick={() => setAlert("")}
+            >
+              <title>Close</title>
+              <path
+                fillRule="evenodd"
+                // eslint-disable-next-line max-len
+                d="M10.293 8l4.147-4.147a1 1 0 011.414 1.414L11.414 9l4.44 4.44a1 1 0 01-1.414 1.414L10.293 10l-4.147 4.147a1 1 0 11-1.414-1.414L8.586 9 4.147 4.56A1 1 0 115.56 3.146L10.293 8z"
+              />
+            </svg>
+          </span>
+        </div>
+      )}
       <form className="mt-16" onClick={handleSubmit}>
         <div className="flex flex-col gap-5">
           <FormField
--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,8 +1,15 @@
-import { surpriseMePrompts } from '../constants';
+import { surpriseMePrompts } from "../constants";
+import { saveAs } from "file-saver";
 
 export const getRandomPrompt = (prompt) => {
   const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
   const randomPrompt = surpriseMePrompts[randomIndex];
   if (randomPrompt === prompt) return getRandomPrompt(prompt);
   return randomPrompt;
+};
+
+export async function downloadImage(_id, photo) {
+  const response = await fetch(photo);
+  const blob = await response.blob();
+  saveAs(blob, `${_id}.jpg`);
 }
